f4eaf31f6b03be7d76f3c935ecfef8f8231cf0b0
Feihong Hsu <feihong.hsu@gmail.com>
2024-02-22 12:08:13 +0100

    On level 4

---
+++ README.md
@@ -0,0 +1,15 @@
+## Feihong's Playthrough of WarriorJS
+
+## Installation
+
+    npm install
+
+## Commands
+
+Play game:
+
+    npx warriorjs
+
+## Links
+
+- [WarriorJS on GitHub](https://github.com/olistic/warriorjs)
---
+++ warriorjs/feihong-baby-steps/Player.js
@@ -0,0 +1,13 @@
+class Player {
+  playTurn(warrior) {
+    if (warrior.feel().isEmpty()) {
+      if (warrior.health() < warrior.maxHealth()) {
+        warrior.rest()
+      } else {
+        warrior.walk()
+      }
+    } else {
+      warrior.attack()
+    }
+  }
+}
